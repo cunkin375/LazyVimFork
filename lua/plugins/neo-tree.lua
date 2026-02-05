c201d50c926ca7d188b5f6cf515ac8afc45ba2e6
ali <159713281+cunkin375@users.noreply.github.com>
2026-02-04 22:38:01 -0500

    add neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    window = {
+        position = "float",
+        poopup = {
+            size = {
+                width = "80%",
+                height = "80%",
+            },
+            position = "50%",
+            border = "rounded"
+        }
+    },
+}
