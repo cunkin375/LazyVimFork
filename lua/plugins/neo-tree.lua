4d6c09064054cef095d7450b3195f5d41c1ae2f0
Ali <jeremywr22@gmail.com>
2026-02-17 22:28:43 -0500

    changes wooo yea woo

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,8 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-    window = {
-        position = "float",
-        poopup = {
-            size = {
-                width = "80%",
-                height = "80%",
-            },
-            position = "50%",
-            border = "rounded"
-        }
+    opts = {
+        window = {
+            position = "float",
+        },
     },
 }
