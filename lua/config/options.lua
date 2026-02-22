4e91627afb7430bda90e6c42f8599834b8d5e83c
Jeremy (Ali) <159713281+cunkin375@users.noreply.github.com>
2026-02-22 08:54:52 -0500

    add extra tweaks to powershell terminal

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -28,6 +28,17 @@ if is_windows then
     else
         vim.o.shell = 'powershell'
     end -- if
+<<<<<<< Updated upstream
+=======
+    vim.opt.shellcmdflag = [[
+                -NoLogo -NoProfile -ExecutionPolicy RemoteSigned 
+                -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;
+                           ]]
+    vim.opt.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.opt.shellquote = ""
+    vim.opt.shellxquote = ""
+>>>>>>> Stashed changes
 else
     vim.opt.shell = "bash"
 end -- if
